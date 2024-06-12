4e80a36120deb7ea62b6a4dabe3f6f46fd0cb516
Julian <julian.andres.aquino33@gmail.com>
2024-06-12 17:53:50 -0300

    update form mobile design

--- src/components/Reservation/Reservation.tsx
+++ src/components/Reservation/Reservation.tsx
@@ -2,12 +2,12 @@ import background from "../../assets/background-form.png";
 
 const Reservation = () => {
   return (
-    <div className="flex justify-end items-center relative overflow-hidden h-screen">
+    <div className="flex p-5 justify-center min-h-screen md:justify-end items-center relative md:overflow-hidden md:h-screen">
       <img
         src={background}
         className="absolute w-full top-0 h-full object-cover"
       />
-      <form className="flex flex-col items-center justify-center z-10 bg-white rounded-md p-10 gap-5 mr-20">
+      <form className="flex flex-col items-center justify-center z-10 bg-white rounded-md p-5 md:p-10 gap-5 md:mr-20">
         <div className="flex flex-col gap-3 items-center justify-center">
           <span className="text-teal-500 text-lg">Te Esperamos</span>
           <h2 className="font-medium text-2xl">Reservación</h2>
@@ -24,7 +24,7 @@ const Reservation = () => {
           className="mb-2 p-2 border rounded w-full"
         />
         <input
-          type="text"
+          type="number"
           placeholder="Telefono"
           className="mb-2 p-2 border rounded w-full"
         />
